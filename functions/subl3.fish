6656a6ddc47064b75379e39ec22e669b9f6495fb
Thom Chiovoloni <chiovolonit@gmail.com>
2022-03-06 12:48:48 -0800

    what the hell, *tabs*?

--- functions/subl3.fish
+++ functions/subl3.fish
@@ -14,13 +14,14 @@
 # Then we'll open that instead. Note that this can be worked around by
 # `subl3 -- <path>`, which will cause the first condition to fail.
 function subl3 --description 'alias subl3=code'
-	set -l args $argv
-	if set -q argv[1] && not set -q argv[2] && test -d "$argv[1]" && not string match -q -- '-*' "$argv[1]"
-		set -l workspaces "$argv[1]"/*.code-workspace
-		if set -q workspaces[1] && not set -q workspaces[2]
-			printf "%snote: opening workspace at `%s` instead (disable with `subl3 -- ...`)%s" (set_color --dim) "$workspaces[1]" (set_color normal) >&2
-			set args $workspaces[1]
-		end
-	end
-	code $args
+    set -l args $argv
+    if set -q argv[1] && not set -q argv[2] && test -d "$argv[1]" && not string match -q -- '-*' "$argv[1]"
+        set -l workspaces "$argv[1]"/*.code-workspace
+        if set -q workspaces[1] && not set -q workspaces[2]
+            # kinda spammy, tbh.
+            # printf "%snote: opening workspace at `%s` instead (disable with `subl3 -- ...`)%s" (set_color --dim) "$workspaces[1]" (set_color normal) >&2
+            set args $workspaces[1]
+        end
+    end
+    code $args
 end
